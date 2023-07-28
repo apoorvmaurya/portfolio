60f0d455deb12b4e33e9eb92bf51b7e8c3aa90b9
Apoorv Maurya <apoorvmauryaapoorv@gmail.com>
2023-07-28 19:03:31 +0530

    new file:   .gitignore new file:   script.js new file:   style.css

---
+++ script.js
@@ -0,0 +1,25 @@
+const design_card_butttons = document.querySelectorAll('.design-card');
+const introduction_text = document.querySelectorAll('.introduction-text');
+
+const single_profile_card = document.querySelectorAll('.single-profile-card');
+
+design_card_butttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        introduction_text.forEach((introduction, introductionIndex) => {
+            if (index === introductionIndex) {
+                introduction.style.display = 'block';
+            } else {
+                introduction.style.display = 'none';
+            }
+        });
+        design_card_butttons.forEach((btn, btnIndex) => {
+            if (index === btnIndex) {
+                btn.classList.add('active');
+            } else {
+                btn.classList.remove('active');
+                
+            }
+        });
+    });
+});
+
