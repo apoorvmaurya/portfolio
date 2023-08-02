653e169b26c1ac147b1ff5da2dbef94b35267ed6
Apoorv Maurya <apoorvmauryaapoorv@gmail.com>
2023-08-02 23:56:34 +0530

    modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -23,3 +23,62 @@ design_card_butttons.forEach((button, index) => {
     });
 });
 
+// ... Your existing JavaScript code ...
+
+// Function to show the main section and start animations
+function showMainSectionAndAnimate() {
+    const mainSection = document.querySelector('main');
+    mainSection.style.display = 'block';
+    playAnimationsOnScroll(); // Start animations when the main section becomes visible
+  }
+  
+  // Add an event listener to trigger showing the main section and animations when the page loads
+  window.addEventListener('load', showMainSectionAndAnimate);
+  
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  
+function getElementPosition(element) {
+    const rect = element.getBoundingClientRect();
+    const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    const elementCenter = rect.left + rect.width / 2;
+  
+    if (elementCenter < viewportWidth / 3) {
+      return 'left';
+    } else if (elementCenter > (2 * viewportWidth) / 3) {
+      return 'right';
+    } else {
+      return 'center';
+    }
+  }
+  
+  // Function to trigger animations based on element position in the viewport
+  function playAnimationsOnScroll() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card) => {
+      if (isElementInViewport(card)) {
+        const position = getElementPosition(card);
+        if (position === 'left') {
+          card.style.animation = 'fadeInLeft 1.5s forwards';
+        } else if (position === 'right') {
+          card.style.animation = 'fadeInRight 1.5s forwards';
+        } else {
+          card.style.animation = 'fadeInCenter 1.5s forwards';
+        }
+      }
+    });
+  }
+  
+  // Add an event listener to trigger the animations on scroll
+  window.addEventListener('scroll', playAnimationsOnScroll);
+  
+  // Trigger the animations when the page loads (in case elements are already visible)
+  playAnimationsOnScroll();
+  
